529426f5faa13732d03bf8d8131da4cd45a26e72
kaz <kazuhiro_xenosaga@hotmail.co.jp>
2023-02-03 00:12:04 +0900

    test1

--- Assets/Dropdown_Click.cs
+++ Assets/Dropdown_Click.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Dropdown_Click : MonoBehaviour{
     private Dropdown Dropdown;
-    //private TextMeshPro TextMeshPro;
     private TMP_Dropdown TextMeshPro;
     private GameObject Plane;
-    // Start is called before the first frame update
     void Start(){
         this.TryGetComponent(out this.Dropdown);
         this.TryGetComponent(out this.TextMeshPro);
